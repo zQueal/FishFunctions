5f792d6ca1cda624acf636e2fed521100b020f40
Zach Queal <zach.queal@gmail.com>
2015-05-05 13:55:41 -0400

    added for loop for multiple addresses.

--- ban.fish
+++ ban.fish
@@ -1,7 +1,5 @@
-function ban -d 'Ban IP address using ufw'
-	if set -q argv
-		ufw insert 1 deny from $argv
-	else
-		echo An IP must be put as an argument.
-	end
+function ban --description 'ban all ip addresses using ufw'
+        for val in $argv
+                ufw insert 1 deny from $val
+        end
 end
